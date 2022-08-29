61e720af8e2a9492dfe6c8799c375963c2cd99f8
dds1985 <jdaniel@togetherdevs.com>
2022-08-29 19:49:23 -0300

    using middleware to overwrite error status

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,7 +9,12 @@ const getGoals = (req, res) => {
 // @route POST /api/goals
 // @access Private
 const setGoal = (req, res) => {
-  res.status(200).json({ message: 'Set goal' });
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field');
+  } else {
+    res.status(200).json({ message: 'Set goal' });
+  }
 };
 
 // @desc Update goal
